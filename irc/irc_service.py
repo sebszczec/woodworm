aa8cb84b3ac916d1623125630144009ce5fefc4d
Slaugh <slaugh@slaugh.pl>
2024-04-17 10:45:58 +0000

    some refactoring

--- irc/irc_service.py
+++ irc/irc_service.py
@@ -6,8 +6,9 @@ from tools import event
 class IRCConnection:
     onConnected = event.Event()
 
-    def __init__(self, server, port, nickname, channel):
+    def __init__(self, server, domain, port, nickname, channel):
         self.server = server
+        self.domain = domain
         self.port = port
         self.nickname = nickname
         self.channel = channel
@@ -15,18 +16,18 @@ class IRCConnection:
         self.logger = logger.Logger()
         self.MSG_LEN = 2048
 
-    def connect(self):
+    async def connect(self):
         self.IRC = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.IRC.connect((self.server, self.port))
 
-    def register_user(self):
-        self.send_data(f'USER {self.nickname} slaugh.pl slaugh.pl :{self.nickname}')
-        self.send_data(f'NICK {self.nickname}')
+    async def register_user(self):
+        await self.send_data(f'USER {self.nickname} {self.domain} {self.domain} :{self.nickname}')
+        await self.send_data(f'NICK {self.nickname}')
 
-    def join_channel(self):
-        self.send_data(f'JOIN {self.channel}')
+    async def join_channel(self):
+        await self.send_data(f'JOIN {self.channel}')
 
-    def send_data(self, command):
+    async def send_data(self, command):
         self.IRC.send(bytes(command + "\n", "UTF-8"))
 
     async def listener(self):
@@ -42,11 +43,11 @@ class IRCConnection:
         ircmsg = ircmsg.strip('\r\n')
         
         if "PRIVMSG" in ircmsg:
-            self.handle_priv_message(ircmsg)
+            await self.handle_priv_message(ircmsg)
             return
         
         if ircmsg.startswith("PING :"):
-            self.handle_ping()
+            await self.handle_ping()
             return
         
         if "End of /MOTD command" in ircmsg or "376" in ircmsg:
@@ -56,10 +57,11 @@ class IRCConnection:
 
         self.logger.log(ircmsg)
 
-    def handle_ping(self):
-        self.IRC.send(bytes("PONG :pingisn", "UTF-8"))
+    async def handle_ping(self):
+        # self.IRC.send(bytes("PONG :pingisn", "UTF-8"))
+        await self.send_data("PONG :pingisn")
                       
-    def handle_priv_message(self, ircmsg):
+    async def handle_priv_message(self, ircmsg):
         name = ircmsg.split('!', 1)[0][1:]
         message = ircmsg.split('PRIVMSG', 1)[1].split(':', 1)[1]
         self.logger.log(f"Name: {name}, Message: {message}")
--- woodworm.py
+++ woodworm.py
@@ -6,19 +6,19 @@ from log import logger
 
 async def irc_onConnected(*args, **kwargs):
     irc_connection = args[0]
-    irc_connection.join_channel()
+    await irc_connection.join_channel()
 
 async def main():
     syslog = logger.Logger()
 
-    irc_connection = irc_service.IRCConnection('slaugh.pl', 2000, 'woodworm', '#vorest')
+    irc_connection = irc_service.IRCConnection('slaugh.pl', 'slaugh.pl', 2000, 'woodworm', '#vorest')
     irc_connection.onConnected.subscribe(irc_onConnected)
 
-    irc_connection.connect()
-    irc_connection.register_user()
+    await irc_connection.connect()
+    await irc_connection.register_user()
     
     while True:
-        await irc_connection.listener()
+        await irc_connection.listen()
         await asyncio.sleep(1)
         syslog.log("I'm still alive!", level=logger.LogLevel.DEBUG)
         
