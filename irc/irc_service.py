90de4814cb1c71fdf359c84630c8cc7dcb69d4ab
Slaugh <slaugh@slaugh.pl>
2024-04-18 09:29:41 +0000

    refactirng in irc code

--- irc/irc_service.py
+++ irc/irc_service.py
@@ -9,6 +9,7 @@ class IRCConnection:
     onSpreadDetected = event.Event()
     onSomeoneLeftChannel = event.Event()
 
+
     def __init__(self, server, domain, port, nickname, channel):
         self.server = server
         self.domain = domain
@@ -20,9 +21,11 @@ class IRCConnection:
         self.MSG_LEN = 2048
         self.isConnected = False
 
+
     async def connect(self):
         self.IRC = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.IRC.connect((self.server, self.port))
+
 
     def is_connected(self):
         if self.IRC is None:
@@ -30,21 +33,26 @@ class IRCConnection:
         
         return self.isConnected
             
+
     async def register_user(self):        
         await self.send_data(f'USER {self.nickname} {self.domain} {self.domain} :{self.nickname}')
         await self.send_data(f'NICK {self.nickname}')
 
+
     async def join_channel(self):        
         await self.send_data(f'JOIN {self.channel}')
 
+
     async def send_data(self, command):
         self.IRC.send(bytes(command + "\n", "UTF-8"))
+
 
     async def send_message(self, message):
         if self.is_connected() is False:
             return
         
         await self.send_data(f'PRIVMSG {self.channel} :{message}')
+
 
     async def listen(self):
         while True:
@@ -71,34 +79,48 @@ class IRCConnection:
                 self.logger.log(ircmsg)
                 continue
 
-            # if "PRIVMSG" in ircmsg:
-            #     await self.handle_priv_message(ircmsg)
-            #     continue
+            await self.handle_channel_commands(ircmsg)
 
-            if "BROADCAST" in ircmsg:
-                await self.handle_broadcast_request()
-                continue
-
-            if "SPREAD" in ircmsg:
-                await self.handle_spread_detected(ircmsg)
-                continue
-
-            if "PART" in ircmsg:
-                await self.handle_part(ircmsg)
-                continue
-
-            if "JOIN" in ircmsg:
-                await self.handle_join()
+            if "PRIVMSG" in ircmsg:
+                if await self.handle_priv_message(ircmsg):
+                    continue
 
             self.logger.log(ircmsg)
+
+
+    async def handle_channel_commands(self, ircmsg):
+        if ircmsg.startswith("PING :"):
+            await self.handle_ping()
+            return True
+
+        if "BROADCAST" in ircmsg:
+            await self.handle_broadcast_request()
+            return True
+
+        if "SPREAD" in ircmsg:
+            await self.handle_spread_detected(ircmsg)
+            return True
+
+        if "PART" in ircmsg:
+            await self.handle_part(ircmsg)
+            return True
+
+        if "JOIN" in ircmsg:
+            await self.handle_join()
+            return True
+
+        return False
+
 
     async def handle_ping(self):
         await self.send_data("PONG :pingisn")
         self.logger.log("PONG :pingisn")
 
+
     async def handle_broadcast_request(self):
         await self.onBroadcastRequested.notify(self)
-                      
+
+
     async def handle_spread_detected(self, ircmsg):
         try:
             ip = ircmsg.split("ip:")[1].split(" ")[0]
@@ -110,9 +132,11 @@ class IRCConnection:
 
         await self.onSpreadDetected.notify(self, ip = ip, port = port, ircNick = ircNick)
 
+
     async def handle_part(self, ircmsg):
         ircNick = ircmsg.split('!', 1)[0][1:]
         await self.onSomeoneLeftChannel.notify(self, ircNick = ircNick)
+
 
     async def handle_join(self):
         self.logger.log("JOIN detected, sending BROADCAST", level=logger.LogLevel.DEBUG)
