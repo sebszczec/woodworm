b9fa92202b327e8ce77d9240ad210e63c1127648
Slaugh <slaugh@slaugh.pl>
2024-04-25 09:55:35 +0000

    colors and fix in logging

--- main.py
+++ main.py
@@ -5,6 +5,29 @@ import asyncio
 import woodworm
 import logging
 
+class CustomFormatter(logging.Formatter):
+
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    green = "\x1b[32;20m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s %(levelname)s: %(message)s (%(filename)s:%(lineno)d)"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: green + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
 if __name__ == "__main__":
     if len(sys.argv) != 8:
         print("Usage: python main.py <pathToFiles> <ircNick> <channel> <domain> <ircServer> <ircServerPort> <tcpPort>")
@@ -21,6 +44,11 @@ if __name__ == "__main__":
 
     
     logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger()
+    handler = logging.StreamHandler()
+    handler.setFormatter(CustomFormatter())
+    logger.removeHandler(logger.handlers[0])
+    logger.addHandler(handler)
 
     worm = woodworm.Woodworm(pathToFiles, ircNick, channel, domain, ircServer, ircServerPort, tcpPort)
     asyncio.run(worm.start(debug=False))
--- woodworm.py
+++ woodworm.py
@@ -1,7 +1,6 @@
 import asyncio
 from time import sleep
 from irc import irc_service
-from log import logger
 from context import context
 from botnet import botnet_database
 from botnet.tcp import tcp_services
@@ -9,11 +8,11 @@ import socket
 import os
 import datetime
 from ftp import ftp_services
+import logging
 
 
 class Woodworm:
     def __init__(self, pathToFiles, ircNick, channel, domain, ircServer, ircServerPort, tcpPort):
-        self.syslog = logger.Logger()
         self.botnetDB = botnet_database.BotnetDatabase()
         self.storageDirectory = pathToFiles
         self.ircNick = ircNick
@@ -66,8 +65,8 @@ class Woodworm:
 
     async def irc_onBroadcastRequested(self, *args, **kwargs):
         irc_connection = args[0]
-        self.syslog.log("Broadcast requested", level=logger.LogLevel.DEBUG)
-        self.syslog.log("SPREADING...", level=logger.LogLevel.DEBUG)
+        logging.debug("Broadcast requested")
+        logging.debug("SPREADING...")
         await irc_connection.send_message(f"SPREAD ip:{self.myContext.get_ip()} port:{self.myContext.get_port()}")
 
 
@@ -76,7 +75,7 @@ class Woodworm:
         ip = kwargs.get('ip')
         port = kwargs.get('port')
         nick = kwargs.get('ircNick')
-        self.syslog.log(f"SPREAD DETECTED: nick:{nick}, ip:{ip} port:{port}", level=logger.LogLevel.DEBUG)
+        logging.debug(f"SPREAD DETECTED: nick:{nick}, ip:{ip} port:{port}")
 
         if self.botnetDB.get_bot(nick) is None:
             bot = context.Context(nick, ip, port)
@@ -87,20 +86,20 @@ class Woodworm:
                 bot.get_tcp_connection().set_download_path(self.storageDirectory)
                 bot.get_tcp_connection().send_command(f"IDENTIFY: {self.ircNick}")
             else:
-                self.syslog.log(f"Failed to TCP connect to bot: nick: {nick}, ip: {ip} port: {port}", level=logger.LogLevel.ERROR)
+                logging.error(f"Failed to TCP connect to bot: nick: {nick}, ip: {ip} port: {port}")
                                  
             self.botnetDB.add_bot(bot)
 
-            self.syslog.log(f"Bot added to DB: nick: {nick}, ip: {ip} port: {port}", level=logger.LogLevel.INFO)
-            self.syslog.log(f"Number of bots: {len(self.botnetDB.get_bots())}", level=logger.LogLevel.INFO)
+            logging.info(f"Bot added to DB: nick: {nick}, ip: {ip} port: {port}")
+            logging.info(f"Number of bots: {len(self.botnetDB.get_bots())}")
             
 
     async def irc_onSomeoneLeftChannel(self, *args, **kwargs):
         nick = kwargs.get('ircNick')
         if self.botnetDB.get_bot(nick) is not None:
             self.botnetDB.remove_bot(self.botnetDB.get_bot(nick))
-            self.syslog.log(f"Bot removed from DB: nick: {nick}", level=logger.LogLevel.INFO)
-            self.syslog.log(f"Number of bots: {len(self.botnetDB.get_bots())}", level=logger.LogLevel.INFO)
+            logging.info(f"Bot removed from DB: nick: {nick}")
+            logging.info(f"Number of bots: {len(self.botnetDB.get_bots())}")
 
 
     async def irc_onCommandLS(self, *args, **kwargs):
@@ -145,11 +144,11 @@ class Woodworm:
 
         connection = bot.get_tcp_connection()
         if connection is None:
-            self.syslog.log(f"Bot {receiver} has no connection, trying to use reversed connection", level=logger.LogLevel.WARNING)
+            logging.warning(f"Bot {receiver} has no connection, trying to use reversed connection")
             connection = bot.get_reversed_tcp_connection()
         
         if connection is None:
-            self.syslog.log(f"Bot {receiver} has no active connections", level=logger.LogLevel.ERROR)
+            logging.error(f"Bot {receiver} has no active connections")
             await irc_connection.send_query(nickname, f"Bot {receiver} has no active connections")
             return  
 
@@ -192,7 +191,7 @@ class Woodworm:
         try:
             files = await asyncio.to_thread(os.listdir, self.storageDirectory)
         except Exception as e:
-            self.syslog.log(f"Error listing files: {str(e)}", level=logger.LogLevel.ERROR)
+            logging.error(f"Error listing files: {str(e)}")
         return files
 
 
@@ -208,7 +207,7 @@ class Woodworm:
             }
             return file_info
         except Exception as e:
-            self.syslog.log(f"Error getting file info: {str(e)}", level=logger.LogLevel.ERROR)
+            logging.error(f"Error getting file info: {str(e)}")
             return None
         
 
@@ -218,19 +217,19 @@ class Woodworm:
         
         bot = self.botnetDB.get_bot(nick)
         if bot is None:
-            self.syslog.log(f"Bot not found: nick: {nick}", level=logger.LogLevel.ERROR)
+            logging.error(f"Bot not found: nick: {nick}")
             await asyncio.sleep(0.5)
             connection.send_command(f"AUTH-REQ {nick}")
             return
         
         bot.set_reversed_tcp_connection(connection)
         bot.get_reversed_tcp_connection().set_download_path(self.storageDirectory)
-        self.syslog.log(f"Reverse connection established with nick: {nick}", level=logger.LogLevel.INFO)
+        logging.info(f"Reverse connection established with nick: {nick}")
 
 
     async def another_loop(self):
         while True:
             await asyncio.sleep(1)
-            # self.syslog.log("I'm still alive!", level=logger.LogLevel.DEBUG) 
+            # logging.debug("I'm still alive!") 
 
     
